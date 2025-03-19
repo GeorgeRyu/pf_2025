dd1b3e17ddf85a16c923567e21415fd7677fa2fd
George <takahashi@in-focus.co.jp>
2025-03-19 18:42:47 +0900

    update_依存関係にmicrocms-js-sdkとsassを追加し、globals.cssを削除してglobals.scssをインポートするように変更。ページコンポーネントを更新し、microCMSからデータを取得する機能を追加。レイアウトのメタデータを更新し、言語を日本語に設定。

---
+++ libs/client.js
@@ -0,0 +1,6 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+    serviceDomain: 'george-ryu', 
+    apiKey: 'ryzNsNU1wqkLqQzSAIRZzTQgHgCTk1Jx8OpI',
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,60 @@
-import Image from "next/image";
+import styles from './page.module.scss'
+import { client } from '@/libs/client'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+type dataType = {
+  contents: contentsType[]
+  totalCount: number
+  offset: number
+  limit: number
+}
+type contentsType = {
+  id: string
+  title: string
+  picture: {
+    url: string,
+    height: number,
+    width: number
+  }
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default async function todo(){  
+  //microCMSからデータを取得する処理
+  const data: dataType = await client.get({
+    endpoint: 'works', //microCMSで設定したもの
+    // queries: { fields: 'title, id, picture' },
+  })
+  
+  return(
+    <>
+      <div className="relative z-30">
+        <header className='grid grid-cols-4 gap-[8px]'>
+          <div className='col-span-2'>
+            <h1 className=''>RYU<br/>TAKAHASHI</h1>
+            <p className='mt-[1em]'>WEB DEVELOPER</p>
+          </div>
+          <div className="col-span-2">sound<span className=''>(off)</span><span>{`{ setting }`}</span></div>
+        </header>
+        <div className='grid grid-cols-4 gap-[8px] mt-[20px]'>
+          <div className='relative col-span-2 col-start-3'>
+            <h2 id='title-contents' className='on-hover'>Works</h2>
+            <div className="absolute top-[40px] left-0">
+              {data?.contents.map((value, index) => (
+                <a href={`/${value.id}`} key={index} className="flex" data-pic={value.picture && value.picture.url}>{value.title}</a>
+              ))}
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+        <footer className='fixed left-0 bottom-0 grid grid-cols-2 grid-rows-3 gap-x-[8px] w-full px-6 pb-8'>
+              <div className="btn-menu col-start-1 row-start-3 pt-[1em]">MENU</div>
+              <div className="flex items-center gap-x-[2em] col-start-2 row-start-1">
+                <a href="http://" target="_blank" rel="noopener noreferrer">Instagram</a>
+                <a href="http://" target="_blank" rel="noopener noreferrer">X</a>
+              </div>
+              <a href="mailto:r.takahasii@gmail.com" className=" col-start-2 row-start-2">r.takahasii@gmail.com</a>
+              <p className='mt-[1em] col-start-2 row-start-3'>{`{`} <span>scroll</span> {`}`}</p>
+        </footer>
+      </div>
+    </>
+  )
 }
